8ed5203f7aacac1cd3147814af85bd89f439ea0b
pato <patriciocabreravera@gmail.com>
2019-08-04 16:29:28 -0400

    Updates

---
+++ precache-manifest.53ac0146bf92e4d78afeeb7809cba6fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d80847050b0a7cf237587e7abb89643c",
+    "url": "/phpato/react-pets/index.html"
+  },
+  {
+    "revision": "f330f6383a11646ca9ed",
+    "url": "/phpato/react-pets/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "59c18c9b7623387c1df0",
+    "url": "/phpato/react-pets/static/js/2.8ca743ae.chunk.js"
+  },
+  {
+    "revision": "f330f6383a11646ca9ed",
+    "url": "/phpato/react-pets/static/js/main.00b4a0dd.chunk.js"
+  },
+  {
+    "revision": "e17160cb62af33d2bca2",
+    "url": "/phpato/react-pets/static/js/runtime~main.2e9a68bc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/phpato/react-pets/static/media/logo.5d5d9eef.svg"
+  }
+]);
